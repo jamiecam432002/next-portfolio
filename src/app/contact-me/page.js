779f998fd8adb72888a9109127fee1d27956dad3
Jamie Cameron <jamiecam43@gmail.com>
2025-05-23 12:30:33 -0400

    fixed grammatical error on contact page

--- src/app/contact-me/page.js
+++ src/app/contact-me/page.js
@@ -5,9 +5,9 @@ export default function ContactPage() {
 		<div>
 			<h1>Contact</h1>
 			<h2>
-				If you'd like the chance to work together or simply want to touch base,
-				you can fill out the form below - or for an even faster response try
-				emailing me @ jamiecam43@gmail.com.
+				If you&apos;d like the chance to work together or simply want to touch
+				base, you can fill out the form below - or for an even faster response
+				try emailing me @ jamiecam43@gmail.com.
 			</h2>
 			<ContactForm />
 		</div>
