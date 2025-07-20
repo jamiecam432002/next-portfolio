ed6da52a9d701840dba2a00b28729a9c20b42f58
James Cameron <jamiecam43@gmail.com>
2025-07-20 09:38:35 -0400

    fixing analytic code

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,12 +24,8 @@ export default async function RootLayout({ children }) {
 					<Header initialUser={currentUser?.toJSON()} />
 					{children}
 				</div>
-				{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID &&
-					process.env.NODE_ENV === 'production' && (
-						<GoogleAnalytics
-							gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
-						/>
-					)}
+
+				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
 			</body>
 		</html>
 	);
