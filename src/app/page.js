c7e26f7189a1b499d9d8a56a885dcaa63753c0b8
James Cameron <jamiecam43@gmail.com>
2025-08-04 10:16:46 -0400

    change to homepage description

--- src/app/page.js
+++ src/app/page.js
@@ -11,15 +11,16 @@ export default async function Home() {
 				<br /> Jamie A Cameron
 			</h1>
 			<h2>
-				I am a fullstack javascript developer in the Guelph/KW area who
-				specializes in developing responsive, lightweight websites & apps using
-				Next.js and React. I build apps using a combination of server and client
-				side rendering to maximize performance in the browser, regardless of
-				device or environment.
+				I am a senior, fullstack javascript developer in the Guelph/KW area who
+				specializes in developing production level websites & apps using Next.js
+				and React. I build apps using a combination of server and client side
+				rendering to maximize performance in the browser, regardless of device
+				or environment.
 			</h2>
 			<h3>
-				If you are looking for a brief summary of the skills I hang my hat on,
-				look no further:
+				As someone who has worked in several positions over the years, and worn
+				a lot of different hats, these are the skills I consider to be my main
+				areas of expertise:
 			</h3>
 			<SkillList type='primary' />
 		</main>
